9056c7fc86a13a226feba739c1b7d8b790fb137e
Can Elmas <Can.Elmas@monitise.com>
2018-08-26 16:09:31 +0300

    Remove setup scripts

--- setup/costs-current-month.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# current usage costs (current month or the past month if below is called on the first day of the month)
-python3 ~/aws-cost-reminder/eyws.py email-costs --profile pozitron --template ~/aws-cost-reminder/email_costs.j2 \
---smtp-host handel.pozitron.com --smtp-port 10025 --smtp-from=aws-cost-reminder@commencis.com \
---emails=can@commencis.com
--- setup/scp_to_terry.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-scp eyws.py requirements.txt templates/email_costs.j2 \
-setup/costs-current-month.sh setup/costs-3months.sh \
-terryadmin@terry.vm.pozitron.com:~/aws-cost-reminder/
